afc74bb4cc189a3889b5fa8cadb025f56ecde347
Yasser Hesham <yasserhesham001@gmail.com>
2025-09-07 08:05:21 +0300

    gg

--- ContactsApi/Program.cs
+++ ContactsApi/Program.cs
@@ -7,8 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 //builder.Services.AddDbContext<AppDbContext>
 //    (options=>options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
